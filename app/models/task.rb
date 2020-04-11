a824a6df4b113359b05db09d6eba1ae34ba07643
fune0 <fune0x8@gmail.com>
2020-04-11 11:54:35 +0900

    タスクモデルにコールバックを実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
   validates :name, presence: true
   validates :name, length: { maximum: 30}
+
+  private
+
+    def set_nameless_name
+      if name.blank?
+        self.name = '名前なし'
+      end
+    end
 end
