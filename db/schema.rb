7da7a7535689a0c1c6e2e082fea5b50dfd5ce60f
fune0 <fune0x8@gmail.com>
2020-04-11 15:49:12 +0900

    ユーザー管理機能の基礎部分の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_043145) do
+ActiveRecord::Schema.define(version: 2020_04_11_051755) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_04_11_043145) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
