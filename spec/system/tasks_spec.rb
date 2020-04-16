b975d20bcc98245996a4933fed11e266b055f15b
fune0 <fune0x8@gmail.com>
2020-04-16 21:36:16 +0900

    追加の共通化する作業

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,25 +1,27 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, nickname: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, nickname: 'ユーザーB', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: 'タスクA', user: user_a) }
+
+  before do
+    FactoryBot.create(:task, name: 'タスクA', user: user_a)
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it { expect(page).to have_content 'タスクA' }
+  end
+
   describe '一覧表示機能' do
-    let(:user_a) { FactoryBot.create(:user, nickname: 'ユーザーA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, nickname: 'ユーザーB', email: 'b@example.com') }
-
-    before do
-      FactoryBot.create(:task, name: 'タスクA', user: user_a)
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
-
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        # 作成済みのタスクの名称が画面上に表示されていることを確認
-        expect(page).to have_content 'タスクA'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
 
     context 'ユーザーBがログインしているとき' do
@@ -29,6 +31,18 @@ describe 'タスク管理機能', type: :system do
         # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
         expect(page).to have_no_content 'タスクA'
       end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
   end
 end
